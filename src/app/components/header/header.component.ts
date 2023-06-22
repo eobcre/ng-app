b99c4b71f876634ec0ee858f90da128121d65998
eobcre <eobcre@gmail.com>
2023-06-22 00:16:36 -0400

    logo path, smooth scroll logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  logoPath = 'assets/logo/logo.png';
 
+  smoothScroll(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
