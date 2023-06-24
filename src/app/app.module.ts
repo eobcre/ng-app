2d0a3c81b170f593163ee1dbe4cf5394586e6f3d
eobcre <eobcre@gmail.com>
2023-06-24 15:14:01 -0400

    fontawesome settings, comments added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
+// modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { WorkComponent } from './components/work/work.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
     AboutComponent,
     WorkComponent,
     ContactComponent,
     FooterComponent,
-    HomeComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
