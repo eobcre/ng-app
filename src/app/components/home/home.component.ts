8e960f990e4872a484d2fe41465746dcbe958db1
eobcre <eobcre@gmail.com>
2023-06-24 15:19:03 -0400

    fontawesome settings

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  faLinkedin,
+  faInstagram,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  faLinkedin = faLinkedin;
+  faInstagram = faInstagram;
+  faGithub = faGithub;
 }
