6b5a8e399e4caa66d46c1df9b433bd3d7e0d00ed
eobcre <eobcre@gmail.com>
2023-06-26 00:54:58 -0400

    created skill data

---
+++ src/skillData.ts
@@ -0,0 +1,46 @@
+export const skillData = [
+  {
+    id: 1,
+    skill: 'HTML5',
+  },
+  {
+    id: 2,
+    skill: 'CSS3',
+  },
+  {
+    id: 3,
+    skill: 'Sass',
+  },
+  {
+    id: 4,
+    skill: 'Tailwind CSS',
+  },
+  {
+    id: 5,
+    skill: 'JavaScript',
+  },
+  {
+    id: 6,
+    skill: 'TypeScript',
+  },
+  {
+    id: 7,
+    skill: 'React',
+  },
+  {
+    id: 8,
+    skill: 'Angular',
+  },
+  {
+    id: 9,
+    skill: 'Node.js',
+  },
+  {
+    id: 10,
+    skill: 'Git',
+  },
+  {
+    id: 11,
+    skill: 'Visual Studio',
+  },
+];
